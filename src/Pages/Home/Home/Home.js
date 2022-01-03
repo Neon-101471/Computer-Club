7025d3c4faacc2a90f1a3eddcde3f44a09692e7a
imran-de <stimran4405@gmail.com>
2022-01-03 09:48:02 +0600

    get in touch add

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import ContactInfo from '../ContactInfo/ContactInfo';
 import RecentEvents from '../RecenEvents/RecentEvents';
 import Workshops from '../Workshops/Workshops';
 
 const Home = () => {
     return (
         <div className='my-5'>
-
             <Workshops></Workshops>
             <RecentEvents></RecentEvents>
+            <ContactInfo />
         </div>
     );
 };
