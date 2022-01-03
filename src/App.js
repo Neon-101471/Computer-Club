128d2727bd3ec846d1a79cd4fb397b7b9eb35d2e
imran-de <stimran4405@gmail.com>
2022-01-03 18:49:42 +0600

    accept route

--- src/App.js
+++ src/App.js
@@ -1,17 +1,4 @@
-<<<<<<< HEAD
 import { Route, Switch } from "react-router-dom";
-import "./App.css";
-import NotFound from "./Pages/404/NotFound";
-import About from "./Pages/About/About";
-import Events from "./Pages/Events/Events";
-import Details from "./Pages/Home/Details/Details";
-import Home from "./Pages/Home/Home/Home";
-import Notice from "./Pages/Notice/Notice";
-import RolesAndRegulation from "./Pages/RolesAndRegulation/RolesAndRegulation";
-import Footer from "./Pages/Shared/Footer/Footer";
-import Navigation from "./Pages/Shared/Navigation/Navigation";
-=======
-import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import AuthProvider from './Contexts/AuthProvider';
 import NotFound from './Pages/404/NotFound';
@@ -22,47 +9,14 @@ import Home from './Pages/Home/Home/Home';
 import Login from './Pages/Login/Login/Login';
 import Register from './Pages/Login/Register/Register';
 import Notice from './Pages/Notice/Notice';
-import Results from './Pages/Results/Results';
 import Footer from './Pages/Shared/Footer/Footer';
 import Navigation from './Pages/Shared/Navigation/Navigation';
->>>>>>> 99482630ff036ddbf379b30b37ddec59815960f1
+import Results from "./Pages/Results/Results";
+import RolesAndRegulation from "./Pages/RolesAndRegulation/RolesAndRegulation";
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <Navigation />
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/home">
-          <Home />
-        </Route>
-        <Route path="/notice">
-          <Notice />
-        </Route>
-        <Route path="/events">
-          <Events />
-        </Route>
-        <Route path='/results'>
-          <Results />
-        </Route>
-        <Route path="/details/:detailsId">
-          <Details></Details>
-        </Route>
-        <Route path="/about">
-          <About></About>
-        </Route>
-        <Route path="/roles">
-          <RolesAndRegulation></RolesAndRegulation>
-        </Route>
-        <Route path="*">
-          <NotFound />
-        </Route>
-      </Switch>
-      <Footer />
-=======
       <AuthProvider>
         <Navigation />
         <Switch>
@@ -78,6 +32,9 @@ function App() {
           <Route path='/notice'>
             <Notice />
           </Route>
+          <Route path='/results'>
+            <Results />
+          </Route>
           <Route path="/details/:detailsId">
             <Details></Details>
           </Route>
@@ -90,13 +47,15 @@ function App() {
           <Route path='/register'>
             <Register></Register>
           </Route>
+          <Route path="/roles">
+            <RolesAndRegulation></RolesAndRegulation>
+          </Route>
           <Route path='*'>
             <NotFound />
           </Route>
         </Switch>
         <Footer />
       </AuthProvider>
->>>>>>> 99482630ff036ddbf379b30b37ddec59815960f1
     </div>
   );
 }
